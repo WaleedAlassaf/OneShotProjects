80aab443eb2ed47667cdfeb1a42cc01004e94159
Waleed Alassaf <Walid.elassaf@gmail.com>
2020-11-15 11:10:52 +0300

    Rotation transform

--- LayersOfFun/LayersOfFun/ViewController.swift
+++ LayersOfFun/LayersOfFun/ViewController.swift
@@ -10,60 +10,71 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        imageView.layer.anchorPoint = CGPoint(x: 1.0, y: 0.5)
+        imageView.layer.cornerRadius = 20.0
+        imageView.layer.masksToBounds = true
+        
+        imageView.layer.borderWidth = 3.0
+        imageView.layer.borderColor = UIColor.orange.cgColor
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
+        let layer = imageView.layer
         
-//        let layer = imageView.layer
-//        let anim = CAKeyframeAnimation(keyPath: "transform")
+        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.transform))
         
-        if topPin.isActive {
-            imageHeight.constant = imageView.frame.height
-            imageHeight.isActive = true
-            topPin.isActive = false
-        }
-        let params = UISpringTimingParameters(mass: 0.5,
-                                              stiffness: 50.0,
-                                              damping: 1.0,
-                                              initialVelocity: CGVector.zero)
+        let initial = layer.transform
+        animation.fromValue = initial
         
-        let animator = UIViewPropertyAnimator(duration: 1.0, timingParameters: params)
-        animator.addAnimations {
-            
-            self.imageHeight.constant = 0.0
-            self.view.layoutIfNeeded()
-        }
+        let angle = CGFloat.pi / 2
+        let rotateAround = CATransform3DRotate(initial, angle, 0.0, 0.0, 1.0)
+        animation.toValue = rotateAround
+        
+        animation.duration = 1.0
+//        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut) //Error here
+//        animation.autoreverses = true
+//        animation.repeatCount = Float.infinity
+        layer.add(animation, forKey: "spin")
+        layer.transform = rotateAround
         
         
-//        let animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeInOut) {
-//            self.imageHeight.constant = -100
+        
+        
+//        // fade out animation
+        
+//        let animation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+//        animation.fromValue = NSNumber(value: 1.0)
+//        animation.toValue = 0.0
+//        animation.duration = 1.0
+//        layer.add(animation, forKey: "disappear")
+//        imageView.alpha = 0.0
+        
+        
+        
+//        if topPin.isActive {
+//            imageHeight.constant = imageView.frame.height
+//            imageHeight.isActive = true
+//            topPin.isActive = false
+//        }
+//        let params = UISpringTimingParameters(mass: 0.5,
+//                                              stiffness: 50.0,
+//                                              damping: 1.0,
+//                                              initialVelocity: CGVector.zero)
+//
+//        let animator = UIViewPropertyAnimator(duration: 1.0, timingParameters: params)
+//        animator.addAnimations {
+//
+//            self.imageHeight.constant = 0.0
 //            self.view.layoutIfNeeded()
 //        }
-        animator.startAnimation()
+//
+////        let animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeInOut) {
+////            self.imageHeight.constant = -100
+////            self.view.layoutIfNeeded()
+////        }
+//        animator.startAnimation()
         
         
-        
-//        var perspective = CATransform3DIdentity
-//        perspective.m34 = -1.0/1000.0
-//        layer.transform = perspective
-//
-//        let txform = layer.transform
-//        let angle = CGFloat(Float.pi)
-//        let rotTxform = CATransform3DRotate(txform, angle, 0.0, 1.0, 0.0)
-//        let rotAndMoveLeft = CATransform3DTranslate(rotTxform, 0.0, 0.0, -100.0)
-//        let rotAndMoveRight = CATransform3DTranslate(rotTxform, 0.0, 0.0, 100.0)
-//
-//
-//        anim.values = [txform, rotTxform, rotAndMoveLeft, rotAndMoveRight, txform, txform]
-//        anim.duration = 2.0
-//        anim.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-////        anim.autoreverses = true
-//        anim.repeatCount = .infinity
-//        layer.add(anim, forKey: "spin")
-//        layer.transform = rotTxform
         
     }
-    
     
 }
